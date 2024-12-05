79b796f340f59bb5bbef4db8db4d3b3f710ee3be
akoua <akouayao225@gmail.com>
2024-12-05 21:14:19 +0100

    deuxieme commit

---
+++ src/main/java/com/formation/MathHelper.java
@@ -0,0 +1,23 @@
+package com.formation;
+
+public class MathHelper {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b + 10;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Divider cannot be zero");
+        }
+        return (double) a / b;
+    }
+}
