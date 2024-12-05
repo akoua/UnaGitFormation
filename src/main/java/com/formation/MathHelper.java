798855b419b7a6ae5a5c9ae0d0893070771ef736
akoua <akouayao225@gmail.com>
2024-12-05 21:20:29 +0100

    troiseme commit

--- src/main/java/com/formation/MathHelper.java
+++ src/main/java/com/formation/MathHelper.java
@@ -11,7 +11,7 @@ public class MathHelper {
     }
 
     public static int multiply(int a, int b) {
-        return a * b;
+        return a * b + 10;
     }
 
     public static double divide(int a, int b) {
