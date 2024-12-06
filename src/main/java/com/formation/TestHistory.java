ef5642430164c271dc91c0140a682b177d2546a6
akoua <akouayao225@gmail.com>
2024-12-06 07:15:24 +0100

    2e switch commit

--- src/main/java/com/formation/TestHistory.java
+++ src/main/java/com/formation/TestHistory.java
@@ -2,7 +2,7 @@ package com.formation;
 
 public class TestHistory {
 
-    public static int add(int a, int b) {
-        return a + b + 45;
+    public static int add(int a, int b, int c) {
+        return a + b + c + 45;
     }
 }
