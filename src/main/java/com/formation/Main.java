df3e4ad4bf7e54e07051dad1e0f9a747b1f1b6e8
akoua <akouayao225@gmail.com>
2024-12-07 12:25:48 +0100

    add a test transit output

--- src/main/java/com/formation/Main.java
+++ src/main/java/com/formation/Main.java
@@ -3,5 +3,6 @@ package com.formation;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! C'est bonjour.");
+        System.out.println("Test transit");
     }
 }
